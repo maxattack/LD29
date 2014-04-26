6de7df3f2178cdf4e2f6fae3b8aedae14e8b7816
petey123567 <pete.angst@gmail.com>
2014-04-26 11:26:16 -0700

    item WIP

--- Assets/Scripts/HeroInput.cs
+++ Assets/Scripts/HeroInput.cs
@@ -7,5 +7,6 @@ public class HeroInput : CustomBehaviour {
 	public bool PressingLeft { get { return Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A); } }
 	public bool PressingRight { get { return Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D); } }
 	public bool PressedJump { get { return Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W); } }
+	public bool PressedItem { get { return Input.GetKeyDown(KeyCode.Z) || Input.GetKeyDown(KeyCode.Return); } }
 		
 }
