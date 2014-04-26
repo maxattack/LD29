74c8b3c3fe77adf0a855f098a3f9f0e8a0cbb870
petey123567 <pete.angst@gmail.com>
2014-04-26 10:25:41 -0700

    debris pooling

--- Assets/Scripts/Debris.cs
+++ Assets/Scripts/Debris.cs
@@ -1,7 +1,67 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Debris : MonoBehaviour {
+
+public class Debris : CustomBehaviour {
+
+	Debris prefab;
+	Debris next;
+
+	public bool IsPrefab { 
+		get { return prefab == null; } 
+	}
+
+
+	public Debris Alloc() {
+		Assert(IsPrefab);
+		
+		Debris result;
+		
+		if (next != null) {
+			
+			// RECYCLE INSTANCE
+			result = next;
+			next = result.next;
+			result.next = null;
+			result.gameObject.SetActive(true);
+			
+		} else {
+			
+			// CREATE NEW INSTANCE
+			result = Dup(this);
+			result.prefab = this;
+			
+		}
+		
+		// RE-INIT INSTANCE
+		result.Init();
+		return result;
+	}
+	
+	void Init() {
+		
+	}
+	
+	public void Release() {
+		
+		if (prefab != null) {
+			
+			// DEACTIVATE AND PREPEND TO PREFAB'S FREELIST
+			gameObject.SetActive(false);
+			next = prefab.next;
+			prefab.next = this;
+			
+			
+		} else if (gameObject) {
+			
+			// THIS OBJECT WAS NOT DYNAMICALLY CREATED
+			Destroy(gameObject);
+			
+		}
+		
+	}
+
+
 
 	// Use this for initialization
 	void Start () {
@@ -14,7 +74,7 @@ public class Debris : MonoBehaviour {
 		totalTime += Time.deltaTime;
 
 		if (totalTime > 5) {
-						Destroy (gameObject);
+			Release();
 				}
 	
 	}
