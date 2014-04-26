1c736a87fe22f9a68d6341a51b9947452ec82538
petey123567 <pete.angst@gmail.com>
2014-04-26 00:41:54 -0700

    digging

---
+++ Assets/Scripts/Debris.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Debris : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	float totalTime = 0;
+	// Update is called once per frame
+	void Update () {
+
+		totalTime += Time.deltaTime;
+
+		if (totalTime > 5) {
+						Destroy (gameObject);
+				}
+	
+	}
+}
