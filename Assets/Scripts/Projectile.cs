04286b9410ac13c4338d8a4b54c188179cdb615e
petey123567 <pete.angst@gmail.com>
2014-04-26 10:14:10 -0700

    rawkits

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+
+
+	Transform xform;
+
+	// Use this for initialization
+	void Start () {
+		xform = transform;
+
+		rigidbody.centerOfMass = new Vector3 (-0.4f, 0, 0);
+
+		initDir.Normalize ();
+
+		rigidbody.rotation = Quaternion.FromToRotation (new Vector3 (1, 0, 0), initDir);
+	}
+
+	public Vector3 initDir = new Vector3(1,0,0);
+	void FixedUpdate()
+	{
+
+		rigidbody.AddForce (new Vector3 (0, -1, 0));
+		rigidbody.AddForce (initDir * 1); 
+
+	}
+
+	// Update is called once per frame
+	float totalTime = 0;
+	void Update () {
+	
+		totalTime += Time.deltaTime;
+
+
+
+	}
+
+
+	void OnCollisionEnter(Collision collision) {
+
+		GameObject obj = collision.collider.gameObject;
+		if (obj) {
+
+			GameObjUserData ud = obj.GetComponent<GameObjUserData>();
+			if(ud )
+			{
+				if(ud.goType == GameObjUserData.GOType.Tile)
+				{
+					int x = (int)obj.transform.position.x;
+					int y = (int)obj.transform.position.y;
+					WorldGen.inst.DigRocket(x,y);
+
+					Destroy (gameObject);
+				}
+			}
+
+				}
+		
+	}
+}
